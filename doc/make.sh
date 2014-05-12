1df0f038724891eb62af54f8277826e75a8803f3
Filip Kofron <filip.kofron.cz@gmail.com>
2014-05-12 19:48:54 +0200

    Fixed more czech stuff.

--- doc/make.sh
+++ doc/make.sh
@@ -4,3 +4,8 @@ bibtex Thesis.aux
 pdfcslatex Thesis.tex
 pdfcslatex Thesis.tex
 pdfcslatex Thesis.tex
+pdfcslatex color.tex
+pdfcslatex color.tex
+pdfcslatex color.tex
+pdfcslatex color.tex
+scp Thesis.pdf color.pdf kofee@halt.cz:/home/kofee/public_html/School/Thesis/
